0e60988fcf2e1ea312cd79f8c89a53e628af4487
Maysa Leocadio <lmaysagomes@gmail.com>
2025-02-13 21:46:08 -0300

    alterando endpoint denovo

--- src/main/java/com/eventos/api/controllers/PessoaController.java
+++ src/main/java/com/eventos/api/controllers/PessoaController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/pessoas")
+@RequestMapping("/pessoa")
 public class PessoaController {
 
     private final PessoaService pessoaService;
